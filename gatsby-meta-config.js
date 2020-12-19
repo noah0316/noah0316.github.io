b7c5cafbcf5b77ae25be7850fc7f760be368bb84
noah0316 <noa316@naver.com>
2020-12-19 15:32:24 +0900

    Update meta-config data

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: `Noah's Dev Log`,
   description: `Blog posted about ...`,
-  author: `[Noah]`,
+  author: `[SeungHyun]`,
   introduction: `👨‍💻 23살 대학생 개발자입니다.\n  Swift를 활용한 iOS개발에 관심이 많지만\n 🛠 기술을 가리지는 않습니다.😊`,
   siteUrl: `https://noah0316.github.io`, // Your blog site url
   social: {
