71a3a5c6d46f5a135272aa764a2988d6393c8e88
Noah <noa316@naver.com>
2021-12-11 19:18:13 +0900

    [Styles] Change Config : Post 컨텐츠 이미지 배경색, max width 변경

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,8 +37,9 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 1200,
+              maxWidth: 650,
               linkImagesToOriginal: false,
+              backgroundColor: 'transparent',
             },
           },
           {
@@ -64,6 +65,7 @@ module.exports = {
           `gatsby-remark-smartypants`,
           `gatsby-remark-autolink-headers`,
           `gatsby-remark-emoji`,
+          `gatsby-plugin-open-graph-images`,
         ],
       },
     },
