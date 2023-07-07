92cb8f046fdfde49f3fdc4b2fa51fa672c0e2ac0
noah0316 <dev.noah0316@gmail.com>
2023-07-07 23:41:31 +0900

    [Sponsor] Hide Buy me a coffee

--- src/html.js
+++ src/html.js
@@ -20,18 +20,18 @@ export default class HTML extends React.Component {
           <noscript>You need to enable JavaScript to run this app.</noscript>
           <div key={`body`} id="___gatsby" dangerouslySetInnerHTML={{ __html: this.props.body }} />
           {this.props.postBodyComponents}
-          <script
-            data-name="BMC-Widget"
-            data-cfasync="false"
-            src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
-            data-id="noah0316"
-            data-description="Support me on Buy me a coffee!"
-            data-message=""
-            data-color="#40DCA5"
-            data-position="Right"
-            data-x_margin="18"
-            data-y_margin="18"
-          ></script>
+          {/*<script*/}
+          {/*  data-name="BMC-Widget"*/}
+          {/*  data-cfasync="false"*/}
+          {/*  src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"*/}
+          {/*  data-id="noah0316"*/}
+          {/*  data-description="Support me on Buy me a coffee!"*/}
+          {/*  data-message=""*/}
+          {/*  data-color="#40DCA5"*/}
+          {/*  data-position="Right"*/}
+          {/*  data-x_margin="18"*/}
+          {/*  data-y_margin="18"*/}
+          {/*></script>*/}
         </body>
       </html>
     )
